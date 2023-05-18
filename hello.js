23a901274d4ccc0882de8a0f286506720af678b7
Rinku Hasija <rishabhasija5454@gmail.com>
2023-05-18 09:06:39 +0530

    test_repo

---
+++ hello.js
@@ -0,0 +1,10 @@
+let array = [2, 5, 7, 8 ,9, 11, 33, 12, 44, 511, 10, 2, ,1]
+
+const mapping = array.map((ele, index) => {
+
+     return (ele *2*9+1)
+
+} )
+
+// console.table(mapping)
+console.error("hello")
